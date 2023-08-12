5706e2c48dfde44369fab82dbd19900a2b08acb9
Johan, Axel REMILIEN <-list>
2023-08-12 04:03:49 +0200

    enable togglefullscreen

---
+++ src/wasm/ScreenSizeManager.cpp
@@ -0,0 +1,20 @@
+#include <ScreenSizeManager.h>
+
+#include <emscripten/val.h>
+
+ScreenSizeManager::ScreenSizeManager(DeviceAccessBase *deviceAccess, QObject *parent)
+    : ScreenSizeManagerBase{deviceAccess, parent}
+{
+    m_enabled = true;
+}
+
+void toggleFullScreen()
+{
+    using emscripten::val;
+    const val document = val::global("document");
+    const val fullscreenElement = document["fullscreenElement"];
+    if (fullscreenElement.isUndefined() || fullscreenElement.isNull())
+        document["documentElement"].call<void>("requestFullscreen");
+    else
+        document.call<void>("exitFullscreen");
+}
