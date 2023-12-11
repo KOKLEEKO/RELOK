f967627a1db4be366f3663d42b3f06b3de7edc51
Johan, Axel REMILIEN <-list>
2023-12-11 17:13:44 +0100

    Fix wasm pipeline and print width/height delta

--- src/wasm/ScreenSizeManager.cpp
+++ src/wasm/ScreenSizeManager.cpp
@@ -5,15 +5,13 @@
 **  details.
 **  Author: Johan, Axel REMILIEN (https://github.com/johanremilien)
 **************************************************************************************************/
+
 #include <ScreenSizeManager.h>
 
 #include <emscripten.h>
 
-ScreenSizeManager::ScreenSizeManager(DeviceAccessBase *deviceAccess, QObject *parent)
-    : ScreenSizeManagerBase{deviceAccess, parent}
-{
-    m_enabled = EM_ASM_INT({ return document.fullscreenEnabled; });
-}
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 extern "C" {
 EMSCRIPTEN_KEEPALIVE
@@ -22,7 +20,36 @@ void updateIsFullScreen(bool isFullScreen)
     static_cast<ScreenSizeManager *>(DeviceAccessBase::instance()->manager<ScreenSizeManagerBase>())
         ->updateIsFullScreen(isFullScreen);
 }
+
+EMSCRIPTEN_KEEPALIVE
+void setSafeInsets(float verticalDelta, float horizontalDelta)
+{
+    auto *ssm = static_cast<ScreenSizeManager *>(
+        DeviceAccessBase::instance()->manager<ScreenSizeManagerBase>());
+    qCDebug(lc) << ssm->name() << verticalDelta << horizontalDelta;
 }
+}
+
+ScreenSizeManager::ScreenSizeManager(DeviceAccessBase *deviceAccess, QObject *parent)
+    : ScreenSizeManagerBase{deviceAccess, parent}
+{
+    m_enabled = EM_ASM_INT({ return document.fullscreenEnabled; });
+
+    /* clang-format off */
+    EM_ASM(
+    {
+        window.addEventListener('resize', () => getInnerSize());
+        getInnerSize();
+    });
+    /* clang-format on */
+}
+
+/* clang-format off */
+EM_JS(void, getInnerSize, (),
+{
+    Module._setSafeInsets(document.body.clientHeight - window.innerHeight, document.body.clientWidth - window.innerWidth);
+});
+/* clang-format on */
 
 void ScreenSizeManager::updateIsFullScreen(bool isFullScreen)
 {
