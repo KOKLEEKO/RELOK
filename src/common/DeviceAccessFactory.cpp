111ccabcf8294219c203c6264aa15b1744e469ce
Johan, Axel REMILIEN <-list>
2024-05-14 16:54:52 +0200

    Fix wrong usage of shared pointers in addManager method

--- src/common/DeviceAccessFactory.cpp
+++ src/common/DeviceAccessFactory.cpp
@@ -45,27 +45,27 @@ DeviceAccessFactory::DeviceAccessFactory() {}
 DeviceAccess *DeviceAccessFactory::create()
 {
     static DeviceAccess *deviceAccess = static_cast<DeviceAccess *>(DeviceAccess::instance());
-    deviceAccess->addManager(std::make_shared<ClockLanguageManager>(deviceAccess));
-    deviceAccess->addManager(std::make_shared<PersistenceManager>(deviceAccess));
-    deviceAccess->addManager(std::make_shared<SpeechManager>(deviceAccess));
-    deviceAccess->addManager(std::make_shared<TranslationManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<ClockLanguageManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<PersistenceManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<SpeechManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<TranslationManager>(deviceAccess));
 
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS) || defined(Q_OS_MACOS) || defined(Q_OS_WASM)
-    deviceAccess->addManager(std::make_shared<ShareContentManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<ShareContentManager>(deviceAccess));
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS) || defined(Q_OS_MACOS)
-    deviceAccess->addManager(std::make_shared<ReviewManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<ReviewManager>(deviceAccess));
 #endif
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS) || defined(Q_OS_WASM)
-    deviceAccess->addManager(std::make_shared<AutoLockManager>(deviceAccess));
-    deviceAccess->addManager(std::make_shared<BatteryManager>(deviceAccess));
-    deviceAccess->addManager(std::make_shared<EnergySavingManager>(deviceAccess));
-    deviceAccess->addManager(std::make_shared<ScreenSizeManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<AutoLockManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<BatteryManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<EnergySavingManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<ScreenSizeManager>(deviceAccess));
 #endif
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
-    deviceAccess->addManager(std::make_shared<ScreenBrightnessManager>(deviceAccess));
-    deviceAccess->addManager(std::make_shared<SpeechManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<ScreenBrightnessManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<SpeechManager>(deviceAccess));
 #ifdef Q_OS_ANDROID
-    deviceAccess->addManager(std::make_shared<SplashScreenManager>(deviceAccess));
+    deviceAccess->addManager(std::make_unique<SplashScreenManager>(deviceAccess));
 #endif
 #endif
 #endif
